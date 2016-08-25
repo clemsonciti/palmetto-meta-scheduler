1ec61d3634ec6a7ed147ca382b7ec45d7273e773
Bhavya Ramakrishna <bramakr@g.clemson.edu>
2016-08-25 14:19:01 -0400

    joblist

--- rsub.py
+++ rsub.py
@@ -10,18 +10,22 @@ from subprocess import Popen, PIPE
 from pprint import pprint
 
 # Global variable for submit command
-jobID    = 0
+#jobID    = 0
+
+#class Job(object):
+#jobID = 0
 
 #Class resource which stores all the information provided by the user
 class Resource(object):
-    __submit_cmd = None
-    __stat_cmd   = None
-    __delete_cmd = None
-    __host_name  = None
-    __user       = None
     
     #Parse the configuration file and store the configuration details in the other file
     def init(self, file):
+        self.submit_cmd = None
+        self.stat_cmd   = None
+        self.delete_cmd = None
+        self.host_name  = None
+        self.user       = None
+        
         dir_name = args.initialize
         if not os.path.isdir(dir_name):
            os.mkdir(dir_name)
@@ -36,38 +40,56 @@ class Resource(object):
     # Submit function: This will take the inputs from the user and submits the
     # job on the palmetto. The list of files needed are copied from the local
     # machine to the palmetto node
-    def Submit(self, inFile, __user, __host_name, __submit_cmd, __stat_cmd):
+    def Submit(self, inFile):
         global jobID
-        path='/home/' + __user
-        HOST=__user + '@' + __host_name
-        CMD_QSUB=__submit_cmd + ' ' + path + '/' + inFile
+        path='/home/' + self.user
+        HOST=self.user + '@' + self.host_name
+        CMD_QSUB=self.submit_cmd + ' ' + path + '/' + inFile
         Popen(['scp', inFile, HOST + ':' + path], shell=False, stdout=PIPE)
         proc = Popen(['ssh', HOST, CMD_QSUB], shell=False, stdout=PIPE)
         jobID = proc.communicate()[0]
-        CMD_QSTAT=__stat_cmd + ' ' + jobID
-        Popen(['ssh', HOST, CMD_QSTAT], shell=False)
+        CMD_QSTAT=self.stat_cmd + ' ' + jobID
+        Popen(['ssh', HOST, CMD_QSTAT], shell=False, stdout=PIPE)
 
     # Delete function: This will take the jobID as the input from the user and
     # deletes the particular job
-    def Delete(self, jobId, __user, __host_name, __delete_cmd):
-        HOST=__user + '@' + __host_name
-        CMD_QDEL=__delete_cmd + ' ' + jobId
-        Popen(['ssh', HOST,  CMD_QDEL], shell=False)
+    def Delete(self, jobId):
+        HOST=self.user + '@' + self.host_name
+        CMD_QDEL=self.delete_cmd + ' ' + jobId
+        Popen(['ssh', HOST,  CMD_QDEL], shell=False, stdout=PIPE)
 
     # Query function: This will take the jobID as the input from the user and
     # obtains the detailed information about the job
-    def Query(self, jobId, __user, __host_name, __stat_cmd):
-        HOST=__user + '@' + __host_name
-        CMD_QSTAT=__stat_cmd  + ' ' + '-xf' + ' ' + jobId
-        Popen(['ssh', HOST, CMD_QSTAT], shell=False)
-
+    def Query(self, jobId):
+        HOST=self.user + '@' + self.host_name
+        CMD_QSTAT=self.stat_cmd  + ' ' + '-xf' + ' ' + jobId
+        Popen(['ssh', HOST, CMD_QSTAT], shell=False, stdout=PIPE)
+    
     # Fetch function: Yet to implement
     def Fetch():
         print("Fetch!!")
+    
+    def Update(self, submit_cmd, stat_cmd, delete_cmd, host_name, user):
+        self.submit_cmd = submit_cmd
+        self.stat_cmd = stat_cmd
+        self.delete_cmd = delete_cmd
+        self.host_name = host_name
+        self.user = user
+    
+    def joblist(self, args):
+       os.chdir(args.to)
+       input_file = args.to + '_map_jobid.csv'
+       with open(input_file, 'r') as f:
+          reader = csv.reader(f)
+          for row in reader:
+             if row[0] == 'temp_jobId':
+                 print(row)
+                 for row in reader:
+                     print(row)
 
 # This function maps the local ID with the respective job id and store the file locally
 def map_job(args):
-    dd = 0
+    dd  = 0
     row = 0
     key = 0
     dir_name = args.to
@@ -78,11 +100,11 @@ def map_job(args):
         writer = csv.writer(open(output_file, "w"))
         dd = 1
         writer.writerow([dd])
-        writer.writerow(["temp_jobId", "jobId"])
+        writer.writerow(["temp_jobId", "cluster_name", "jobId"])
         if args.cmd == 'submit':
-            writer.writerow([dd, jobID])
+            writer.writerow([dd, args.to, jobID])
         else:
-            writer.writerow([dd, args.jobId])
+            writer.writerow([dd, args.to, args.jobId])
     else:
         with open(output_file, 'r+') as csvfile:
             reader = csv.reader(csvfile)
@@ -95,9 +117,9 @@ def map_job(args):
                 with open(output_file, 'a+') as csvfile:
                     writer = csv.writer(csvfile)
                     if args.cmd == 'submit':
-                        writer.writerow([key, jobID])
+                        writer.writerow([key, args.to, jobID])
                     else:
-                        writer.writerow([key, args.jobId])
+                        writer.writerow([key, args.to, args.jobId])
                 break
     os.chdir('..')
 
@@ -108,21 +130,22 @@ r = Resource()
 # query the job
 parser = argparse.ArgumentParser(description='Enter one of the following commands')
 subparsers = parser.add_subparsers(help = 'sub-command help', dest='cmd')
-parser_initialize = subparsers.add_parser('initialize', help='initialize help')
-parser_initialize.add_argument('initialize', action="store")
-parser_initialize.add_argument('--configFile', type=argparse.FileType('r'), required = True)
+parser_initialize = subparsers.add_parser('initialize', description='Usage: python rsub.py initialize <cluster_name> --configFile <confile file>')
+parser_initialize.add_argument('initialize', action="store", help='The name of the cluster')
+parser_initialize.add_argument('--configFile', type=argparse.FileType('r'), required = True, help='The config file should be a .json file which contains configuration details to initialize the cluster')
 
-parser_submit = subparsers.add_parser('submit', help='submit help')
-parser_submit.add_argument('--to', action="store", required = True)
-parser_submit.add_argument('--inFile', action="store", required = True)
+parser_submit = subparsers.add_parser('submit', description='Usage: python rsub.py submit --to <cluster_name> --inFile <input file>')
+parser_submit.add_argument('--to', action="store", required = True, help='The name of the cluster on which the job has to be submitted')
+parser_submit.add_argument('--inFile', action="store", required = True, help='The script file which contains the commands which needs to be executed')
 
-parser_delete = subparsers.add_parser('delete', help='delete help')
-parser_delete.add_argument('--to', action="store", required = True)
-parser_delete.add_argument('--jobId', action="store", required = True)
+parser_delete = subparsers.add_parser('delete', description='Usage: python rsub.py delete --jobId <ID of the job>')
+parser_delete.add_argument('--jobId', action="store", required = True, help='The ID of the job which needs to be deleted')
 
-parser_query = subparsers.add_parser('query', help='query help')
-parser_query.add_argument('--to', action="store", required = True)
-parser_query.add_argument('--jobId', action="store", required = True)
+parser_query = subparsers.add_parser('query', description='Usage: python rsub.py query --jobId <ID of the job>')
+parser_query.add_argument('--jobId', action="store", required = True, help='The ID of the job whose information is required')
+
+parser_joblist = subparsers.add_parser('joblist', description='Usage: python rsub.py joblist --to <cluster name>')
+parser_joblist.add_argument('--to', action="store", required = True, help='The name of the cluster on which the job has to be submitted')
 
 args = parser.parse_args()
 
@@ -134,17 +157,15 @@ else:
     with open(input_file, 'r') as f:
         reader = csv.DictReader(f)
         for row in reader:
-            r.__submit_cmd = row['submit_cmd']
-            r.__stat_cmd   = row['stat_cmd']
-            r.__delete_cmd = row['delete_cmd']
-            r.__host_name  = row['host_name']
-            r.__user       = row['user']
+            r.Update(row['submit_cmd'], row['stat_cmd'], row['delete_cmd'], row['host_name'], row['user'])
     os.chdir('..')
     if args.cmd == 'submit':
-        r.Submit(args.inFile, r.__user, r.__host_name, r.__submit_cmd, r.__stat_cmd)
+        r.Submit(args.inFile)
         map_job(args)
     elif args.cmd == 'delete':
-        r.Delete(args.jobId, r.__user, r.__host_name, r.__delete_cmd)
+        r.Delete(args.jobId)
     elif args.cmd == 'query':
-        r.Query(args.jobId, r.__user, r.__host_name, r.__stat_cmd)
+        r.Query(args.jobId)
+    elif args.cmd == 'joblist':
+        r.joblist(args)
 
