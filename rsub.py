9fcf7b3a261c22911abd6e799d08e2469645974c
Bhavya Ramakrishna <bramakr@g.clemson.edu>
2016-09-05 15:48:01 -0400

    modular code

--- rsub.py
+++ rsub.py
@@ -9,14 +9,52 @@ import argparse
 from subprocess import Popen, PIPE
 from pprint import pprint
 
-# Global variable for submit command
-#jobID    = 0
+# Job class will contain all details about a given job with local and remote information.
+# All elements needed to retrive all information about given job.
+class Job(object):
+    jobID = 0
 
-#class Job(object):
-#jobID = 0
+# Type of scheduler that given resource is using e.g. Slurm, Torque,PBS Pro, Condor ...
+# Commands for given resource will depend on the scheduler type. Generic class
+# will support user defined commands. Specialized class available for predefined types
+# of schedulers.
+class Scheduler(object):
+    
+    # Update function: All the values of member variables are updated with the values
+    # stored in the file
+    def Update(self, submit_cmd, stat_cmd, delete_cmd, host_name, user):
+        self.submit_cmd = submit_cmd
+        self.stat_cmd   = stat_cmd
+        self.delete_cmd = delete_cmd
+        self.host_name  = host_name
+        self.user       = user
+    
+    # Submit function: This will take the inputs from the user and submits the
+    # job on the palmetto. The list of files needed are copied from the local
+    # machine to the palmetto node
+    def Submit(self, args):
+        path       = '/home/' + self.user
+        HOST       = self.user + '@' + self.host_name
+        CMD_QSUB   = self.submit_cmd + ' ' + path + '/' + args.inFile
+        Popen(['scp', args.inFile, HOST + ':' + path], shell=False, stdout=PIPE)
+        proc       = Popen(['ssh', HOST, CMD_QSUB], shell=False, stdout=PIPE)
+        Job_.jobID = proc.communicate()[0]
+        CMD_QSTAT  = self.stat_cmd + ' ' + Job_.jobID
+        Popen(['ssh', HOST, CMD_QSTAT], shell=False, stdout=PIPE)
+        Logger_.map_job(args)
+    
+    # Delete function: This will take the jobID as the input from the user and
+    # deletes the particular job
+    def Delete(self):
+        HOST     = self.user + '@' + self.host_name
+        CMD_QDEL = self.delete_cmd + ' ' + Job_.jobID
+        Popen(['ssh', HOST,  CMD_QDEL], shell=False, stdout=PIPE)
 
-#Class resource which stores all the information provided by the user
+# Main class defining cluster (computing platform) resource, every element
+# is representing different cluster (special handling for Open Science Grid).
 class Resource(object):
+    
+    #Scheduler_ = Scheduler()
     
     #Parse the configuration file and store the configuration details in the other file
     def init(self, file):
@@ -26,105 +64,72 @@ class Resource(object):
         self.host_name  = None
         self.user       = None
         
-        dir_name = args.initialize
-        if not os.path.isdir(dir_name):
-           os.mkdir(dir_name)
-
-        os.chdir(dir_name)
-        out_file = csv.writer(open(dir_name + ".csv", "w"))
-        data = json.load(file)
+        out_file = csv.writer(open("master_config.csv", "a+"))
+        data     = json.load(file)
         out_file.writerow(["submit_cmd", "stat_cmd", "delete_cmd", "host_name", "user"])
         out_file.writerow([data['submit_cmd'], data['stat_cmd'], data['delete_cmd'], data['host_name'], data['user']])
-        os.chdir('..')
+
+# Class responsible for logging information about jobs
+class Logger(object):
+    
+    # This function maps the local ID with the respective job id and store the file locally
+    def map_job(self, args):
+        dd       = 0
+        row      = 0
+        key      = 0
+        dir_name = args.to
         
-    # Submit function: This will take the inputs from the user and submits the
-    # job on the palmetto. The list of files needed are copied from the local
-    # machine to the palmetto node
-    def Submit(self, inFile):
-        global jobID
-        path='/home/' + self.user
-        HOST=self.user + '@' + self.host_name
-        CMD_QSUB=self.submit_cmd + ' ' + path + '/' + inFile
-        Popen(['scp', inFile, HOST + ':' + path], shell=False, stdout=PIPE)
-        proc = Popen(['ssh', HOST, CMD_QSUB], shell=False, stdout=PIPE)
-        jobID = proc.communicate()[0]
-        CMD_QSTAT=self.stat_cmd + ' ' + jobID
-        Popen(['ssh', HOST, CMD_QSTAT], shell=False, stdout=PIPE)
+        if not os.path.isdir(dir_name):
+            os.mkdir(dir_name)
+        
+        os.chdir(dir_name)
+        output_file = args.to + "_map_jobid.csv"
 
-    # Delete function: This will take the jobID as the input from the user and
-    # deletes the particular job
-    def Delete(self, jobId):
-        HOST=self.user + '@' + self.host_name
-        CMD_QDEL=self.delete_cmd + ' ' + jobId
-        Popen(['ssh', HOST,  CMD_QDEL], shell=False, stdout=PIPE)
-
-    # Query function: This will take the jobID as the input from the user and
-    # obtains the detailed information about the job
-    def Query(self, jobId):
-        HOST=self.user + '@' + self.host_name
-        CMD_QSTAT=self.stat_cmd  + ' ' + '-xf' + ' ' + jobId
-        Popen(['ssh', HOST, CMD_QSTAT], shell=False, stdout=PIPE)
-    
-    # Fetch function: Yet to implement
-    def Fetch():
-        print("Fetch!!")
-    
-    def Update(self, submit_cmd, stat_cmd, delete_cmd, host_name, user):
-        self.submit_cmd = submit_cmd
-        self.stat_cmd = stat_cmd
-        self.delete_cmd = delete_cmd
-        self.host_name = host_name
-        self.user = user
-    
-    def joblist(self, args):
-       os.chdir(args.to)
-       input_file = args.to + '_map_jobid.csv'
-       with open(input_file, 'r') as f:
-          reader = csv.reader(f)
-          for row in reader:
-             if row[0] == 'temp_jobId':
-                 print(row)
-                 for row in reader:
-                     print(row)
-
-# This function maps the local ID with the respective job id and store the file locally
-def map_job(args):
-    dd  = 0
-    row = 0
-    key = 0
-    dir_name = args.to
-    os.chdir(dir_name)
-    output_file = args.to + "_map_jobid.csv"
-
-    if not os.path.isfile(output_file):
-        writer = csv.writer(open(output_file, "w"))
-        dd = 1
-        writer.writerow([dd])
-        writer.writerow(["temp_jobId", "cluster_name", "jobId"])
-        if args.cmd == 'submit':
-            writer.writerow([dd, args.to, jobID])
+        if not os.path.isfile(output_file):
+            writer = csv.writer(open(output_file, "w"))
+            dd = 1
+            writer.writerow([dd])
+            writer.writerow(["temp_jobId", "cluster_name", "jobId"])
+            writer.writerow([dd, dir_name, Job_.jobID])
         else:
-            writer.writerow([dd, args.to, args.jobId])
-    else:
-        with open(output_file, 'r+') as csvfile:
-            reader = csv.reader(csvfile)
-            for row in reader:
-                key = row[0]
-                key = int(key)
-                key += 1
-                csvfile.seek(0, 0)
-                csvfile.write(str(key))
-                with open(output_file, 'a+') as csvfile:
-                    writer = csv.writer(csvfile)
-                    if args.cmd == 'submit':
-                        writer.writerow([key, args.to, jobID])
-                    else:
-                        writer.writerow([key, args.to, args.jobId])
-                break
-    os.chdir('..')
+            with open(output_file, 'r+') as csvfile:
+                reader = csv.reader(csvfile)
+                for row in reader:
+                    key = row[0]
+                    key = int(key)
+                    key += 1
+                    csvfile.seek(0, 0)
+                    csvfile.write(str(key))
+                    with open(output_file, 'a+') as csvfile:
+                        writer = csv.writer(csvfile)
+                        writer.writerow([key, dir_name, Job_.jobID])
+                    break
+        os.chdir('..')
 
-#Creating an object of the class Resource
-r = Resource()
+# Class designed to store and analyze all information about jobs run so far
+class History(object):
+    
+    def Joblist(self, args):
+        os.chdir(args.to)
+        input_file = args.to + '_map_jobid.csv'
+        with open(input_file, 'r') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                if row[0] == 'temp_jobId':
+                    print(row)
+                    for row in reader:
+                        print(row)
+        os.chdir('..')
+
+# Global configuration class
+#class Config(object):
+
+# Creating an object of the classes
+Resource_ = Resource()
+Schduler_ = Scheduler()
+History_  = History()
+Logger_   = Logger()
+Job_      = Job()
 
 # Obtaining input from the user either to submit the jobs, delete the job or
 # query the job
@@ -152,20 +157,16 @@ args = parser.parse_args()
 if args.cmd == 'initialize':
     r.init(args.configFile)
 else:
-    os.chdir(args.to)
-    input_file = args.to + '.csv'
+    input_file = "master_config.csv"
     with open(input_file, 'r') as f:
         reader = csv.DictReader(f)
         for row in reader:
-            r.Update(row['submit_cmd'], row['stat_cmd'], row['delete_cmd'], row['host_name'], row['user'])
-    os.chdir('..')
+            Schduler_.Update(row['submit_cmd'], row['stat_cmd'], row['delete_cmd'], row['host_name'], row['user'])
     if args.cmd == 'submit':
-        r.Submit(args.inFile)
-        map_job(args)
+       Schduler_.Submit(args)
     elif args.cmd == 'delete':
-        r.Delete(args.jobId)
+        Schduler_.Delete()
     elif args.cmd == 'query':
-        r.Query(args.jobId)
+        Schduler_.Query()
     elif args.cmd == 'joblist':
-        r.joblist(args)
-
+        History_.Joblist(args)
