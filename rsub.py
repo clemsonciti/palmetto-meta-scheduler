fb0a2ca340786fa674a31bd98eeefc2ce83b6f49
Bhavya Ramakrishna <bramakr@g.clemson.edu>
2016-09-13 18:07:15 -0400

    updated code

--- rsub.py
+++ rsub.py
@@ -1,3 +1,4 @@
+import fileinput
 import sys
 import os
 import os.path
@@ -12,7 +13,7 @@ from pprint import pprint
 # Job class will contain all details about a given job with local and remote information.
 # All elements needed to retrive all information about given job.
 class Job(object):
-    jobID = 0
+    jobId = 0
 
 # Type of scheduler that given resource is using e.g. Slurm, Torque,PBS Pro, Condor ...
 # Commands for given resource will depend on the scheduler type. Generic class
@@ -22,52 +23,60 @@ class Scheduler(object):
     
     # Update function: All the values of member variables are updated with the values
     # stored in the file
-    def Update(self, submit_cmd, stat_cmd, delete_cmd, host_name, user):
-        self.submit_cmd = submit_cmd
-        self.stat_cmd   = stat_cmd
-        self.delete_cmd = delete_cmd
-        self.host_name  = host_name
-        self.user       = user
+    def Update(self, clusterName, submitCmd, statCmd, deleteCmd, hostName, user):
+        self.clusterName = clusterName
+        self.submitCmd   = submitCmd
+        self.statCmd     = statCmd
+        self.deleteCmd   = deleteCmd
+        self.hostName    = hostName
+        self.user        = user
     
     # Submit function: This will take the inputs from the user and submits the
     # job on the palmetto. The list of files needed are copied from the local
     # machine to the palmetto node
     def Submit(self, args):
         path       = '/home/' + self.user
-        HOST       = self.user + '@' + self.host_name
-        CMD_QSUB   = self.submit_cmd + ' ' + path + '/' + args.inFile
-        Popen(['scp', args.inFile, HOST + ':' + path], shell=False, stdout=PIPE)
-        proc       = Popen(['ssh', HOST, CMD_QSUB], shell=False, stdout=PIPE)
-        Job_.jobID = proc.communicate()[0]
-        CMD_QSTAT  = self.stat_cmd + ' ' + Job_.jobID
-        Popen(['ssh', HOST, CMD_QSTAT], shell=False, stdout=PIPE)
-        Logger_.map_job(args)
+        host       = self.user + '@' + self.hostName
+        splitJobScriptLocation = args.inFile.split('/')
+        inputFile = splitJobScriptLocation[len(splitJobScriptLocation)-1]
+        qsubCmd    = self.submitCmd + ' ' + path + '/' + inputFile
+        Popen(['scp', inputFile, host + ':' + path], shell=False, stdout=PIPE)
+        proc       = Popen(['ssh', host, qsubCmd], shell=False, stdout=PIPE)
+        Job_.jobId = proc.communicate()[0]
+        qstatCmd  = self.statCmd + ' ' + Job_.jobId
+        Popen(['ssh', host, qstatCmd], shell=False, stdout=PIPE)
+        if Job_.jobId != '0':
+            Logger_.map_job(args)
     
     # Delete function: This will take the jobID as the input from the user and
     # deletes the particular job
     def Delete(self):
-        HOST     = self.user + '@' + self.host_name
-        CMD_QDEL = self.delete_cmd + ' ' + Job_.jobID
-        Popen(['ssh', HOST,  CMD_QDEL], shell=False, stdout=PIPE)
+        host     = self.user + '@' + self.hostName
+        qdelCmd  = self.deleteCmd + ' ' + Job_.jobId
+        Popen(['ssh', host,  qdelCmd], shell=False, stdout=PIPE)
 
 # Main class defining cluster (computing platform) resource, every element
 # is representing different cluster (special handling for Open Science Grid).
 class Resource(object):
     
-    #Scheduler_ = Scheduler()
-    
     #Parse the configuration file and store the configuration details in the other file
     def init(self, file):
-        self.submit_cmd = None
-        self.stat_cmd   = None
-        self.delete_cmd = None
-        self.host_name  = None
-        self.user       = None
+        self.ClusterName = None
+        self.submitCmd   = None
+        self.statCmd     = None
+        self.deleteCmd   = None
+        self.hostName    = None
+        self.user        = None
         
-        out_file = csv.writer(open("master_config.csv", "a+"))
-        data     = json.load(file)
-        out_file.writerow(["submit_cmd", "stat_cmd", "delete_cmd", "host_name", "user"])
-        out_file.writerow([data['submit_cmd'], data['stat_cmd'], data['delete_cmd'], data['host_name'], data['user']])
+        if not os.path.isfile("master_config.csv"):
+            out_file = csv.writer(open("master_config.csv", "w"))
+            data     = json.load(file)
+            out_file.writerow(["clusterName", "submitCmd", "statCmd", "deleteCmd", "hostName", "user"])
+            out_file.writerow([data['clusterName'], data['submitCmd'], data['statCmd'], data['deleteCmd'], data['hostName'], data['user']])
+        else:
+            out_file = csv.writer(open("master_config.csv", "a+"))
+            data     = json.load(file)
+            out_file.writerow([data['clusterName'], data['submitCmd'], data['statCmd'], data['deleteCmd'], data['hostName'], data['user']])
 
 # Class responsible for logging information about jobs
 class Logger(object):
@@ -89,8 +98,8 @@ class Logger(object):
             writer = csv.writer(open(output_file, "w"))
             dd = 1
             writer.writerow([dd])
-            writer.writerow(["temp_jobId", "cluster_name", "jobId"])
-            writer.writerow([dd, dir_name, Job_.jobID])
+            writer.writerow(["tempJobId", "clusterName", "jobId", "scriptFiles"])
+            writer.writerow([dd, dir_name, Job_.jobId, args.inFile])
         else:
             with open(output_file, 'r+') as csvfile:
                 reader = csv.reader(csvfile)
@@ -102,7 +111,7 @@ class Logger(object):
                     csvfile.write(str(key))
                     with open(output_file, 'a+') as csvfile:
                         writer = csv.writer(csvfile)
-                        writer.writerow([key, dir_name, Job_.jobID])
+                        writer.writerow([key, dir_name, Job_.jobId, args.inFile])
                     break
         os.chdir('..')
 
@@ -115,7 +124,7 @@ class History(object):
         with open(input_file, 'r') as f:
             reader = csv.reader(f)
             for row in reader:
-                if row[0] == 'temp_jobId':
+                if row[0] == 'tempJobId':
                     print(row)
                     for row in reader:
                         print(row)
@@ -133,37 +142,47 @@ Job_      = Job()
 
 # Obtaining input from the user either to submit the jobs, delete the job or
 # query the job
-parser = argparse.ArgumentParser(description='Enter one of the following commands')
-subparsers = parser.add_subparsers(help = 'sub-command help', dest='cmd')
-parser_initialize = subparsers.add_parser('initialize', description='Usage: python rsub.py initialize <cluster_name> --configFile <confile file>')
-parser_initialize.add_argument('initialize', action="store", help='The name of the cluster')
+parser = argparse.ArgumentParser(description=' A wrapper to provide commands to the scheduler to execute certain task. The first task is to initialize the'
+                                             ' cluster on which the job execution should take place. A user can submit a job, delete a job, query for the status'
+                                             ' of the job, get the list of all the jobs submitted'
+                                             ' For example to initialize a cluster - Usage: python rsub.py initialize -h')
+subparsers = parser.add_subparsers(dest='cmd')
+parser_initialize = subparsers.add_parser('initialize', description=' Initialize is used to intialize the clusters. The clusters can be palmetto, OSG etc.Usage: python rsub.py initialize'
+                                                                    ' <cluster_name> --configFile <confile file>')
+parser_initialize.add_argument('initialize', metavar='<clusterName>', help='The name of the cluster')
 parser_initialize.add_argument('--configFile', type=argparse.FileType('r'), required = True, help='The config file should be a .json file which contains configuration details to initialize the cluster')
 
-parser_submit = subparsers.add_parser('submit', description='Usage: python rsub.py submit --to <cluster_name> --inFile <input file>')
-parser_submit.add_argument('--to', action="store", required = True, help='The name of the cluster on which the job has to be submitted')
-parser_submit.add_argument('--inFile', action="store", required = True, help='The script file which contains the commands which needs to be executed')
+parser_submit = subparsers.add_parser('submit', description=' A utility that allows you to submit a job to the cluster. When the user submits a job using the job script, the script will'
+                                                            ' be parsed and job will be sent to the scheduler. Usage: python rsub.py submit --to <cluster_name> --inFile <input file>')
+parser_submit.add_argument('--to', metavar='<clusterName>', required = True, help='The name of the cluster on which the job has to be submitted')
+parser_submit.add_argument('--inFile', metavar='<inputFile pbs format>', required = True, help='The script file which contains the commands which needs to be executed')
 
-parser_delete = subparsers.add_parser('delete', description='Usage: python rsub.py delete --jobId <ID of the job>')
-parser_delete.add_argument('--jobId', action="store", required = True, help='The ID of the job which needs to be deleted')
+parser_delete = subparsers.add_parser('delete', description=' A utility that allows you to delete a job that was submitted using submit command.'
+                                                            ' Usage: python rsub.py delete --jobId <ID of the job>')
+parser_delete.add_argument('--jobId', metavar='<Local jobId>', required = True, help='The ID of the job which needs to be deleted')
 
-parser_query = subparsers.add_parser('query', description='Usage: python rsub.py query --jobId <ID of the job>')
-parser_query.add_argument('--jobId', action="store", required = True, help='The ID of the job whose information is required')
+parser_query = subparsers.add_parser('query', description=' A utility that provides you the status of all the jobs that has been submitted using submit command. Usage: python rsub.py'
+                                                          ' query --jobId <ID of the job>')
+parser_query.add_argument('--jobId', metavar='<Local jobId>', required = True, help='The ID of the job whose information is required')
 
-parser_joblist = subparsers.add_parser('joblist', description='Usage: python rsub.py joblist --to <cluster name>')
-parser_joblist.add_argument('--to', action="store", required = True, help='The name of the cluster on which the job has to be submitted')
+parser_joblist = subparsers.add_parser('joblist', description=' A utility that provides the history of all the jobs which has been submitted on all the clusters.'
+                                                              ' Usage: python rsub.py joblist --to <cluster name>')
+parser_joblist.add_argument('--to', metavar='<clusterName>', required = True, help='The name of the cluster on which the job has to be submitted')
 
 args = parser.parse_args()
 
 if args.cmd == 'initialize':
-    r.init(args.configFile)
+    Resource_.init(args.configFile)
 else:
-    input_file = "master_config.csv"
-    with open(input_file, 'r') as f:
+    configFileLocation = str("master_config.csv")
+    with open(configFileLocation, 'r') as f:
         reader = csv.DictReader(f)
         for row in reader:
-            Schduler_.Update(row['submit_cmd'], row['stat_cmd'], row['delete_cmd'], row['host_name'], row['user'])
+            if row['clusterName'] == args.to:
+                Schduler_.Update(row['clusterName'], row['submitCmd'], row['statCmd'], row['deleteCmd'], row['hostName'], row['user'])
+                break;
     if args.cmd == 'submit':
-       Schduler_.Submit(args)
+        Schduler_.Submit(args)
     elif args.cmd == 'delete':
         Schduler_.Delete()
     elif args.cmd == 'query':
